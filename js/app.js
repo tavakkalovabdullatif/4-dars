1afb2bd8218cc9c70e2addce62fb9e6522de09d0
abdullatif <abdullatif>
2025-08-01 16:23:25 +0500

    frebtm

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+console.log(Math.trunc(Math.random() * 5) + 1);
+
+console.log(Math.trunc(Math.random() * 7) + 1);
+
+console.log(Math.trunc(Math.random() * 19) + 1);
+
+console.log(Math.trunc(Math.random() * 25) + 1);
+
+console.log(Math.trunc(Math.random() * 48) + 1);
+
+console.log(Math.trunc(Math.random() * 76) + 1);
+
+console.log(Math.trunc(Math.random() * 65) + 1);
+
+console.log(Math.trunc(Math.random() * 97) + 1);
+
+console.log(Math.trunc(Math.random() * 66) + 1);
+
+console.log(Math.trunc(Math.random() * 99) + 1);
+
+console.log(Math.trunc(Math.random() * 93) + 1);
+
+console.log(Math.trunc(Math.random() * 39) + 1);
+
+console.log(Math.trunc(Math.random() * 55) + 1);
+
+console.log(Math.trunc(Math.random() * 44) + 1);
+
+
+
